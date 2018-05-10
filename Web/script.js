bcd193481d8bed90d86c0d05e1b6e30b0c0861c1
zombie24x7 <androidshubham@gmail.com>
2018-05-10 18:34:47 +0530

    JS

--- Web/script.js
+++ Web/script.js
@@ -1,3 +1,4 @@
+
 console.log('Hello');
 
 var name = 'Shubham!'; //String
@@ -61,3 +62,175 @@ display(sum2);
 function display(sum){
   console.log(sum);
 }
+var p = 100;
+someFun(p);
+function someFun(p){
+  console.log(p);
+}
+
+var names = ['john','kevin','jack'];
+var years = new Array(1,2,3);
+console.log(years[0]);
+console.log(names[0]);
+console.log(names.length);
+names[1] = 'Ben';
+console.log(names);
+
+var john = ['John', 'Smith', 1999, 'Designer', false];
+john.push('blue');
+john.unshift('Mr. ')
+john.pop();
+john.shift();
+console.log(john);
+
+//alert(john.indexOf('Teacher'));
+
+if (john.indexOf('Teacher') === -1) {
+  console.log("John is not a Teacher");
+} else {
+  console.log('He is a Desiger');
+}
+
+var john = {
+  name : 'John',
+  lastName : 'Smith',
+  yearOfBirth : 1990,
+  job : 'Teacher',
+  isMarried : false
+};
+console.log(john);
+console.log(john.lastName);
+console.log(john['lastName']);
+
+var xyz = 'job';
+console.log(john[xyz]);
+
+john.lastName = 'Miller';
+john.job = 'Programmer';
+console.log(john);
+var jane = new Object();
+jane.name = 'Jane';
+jane.lastName = 'Smith';
+jane['yearOfBirth'] = 1970;
+jane['job'] = 'retired';
+jane['isMarried'] = true;
+
+console.log(jane);
+//v1.0
+/*
+var james = {
+  name : 'James',
+  lastName : 'Smith',
+  yearOfBirth : 1990,
+  job : 'Doctor',
+  isMarried : false,
+  family : ['Jane' , 'Mark' , 'Bob'],
+  calculateAge : function(){
+    return 2018 - this.yearOfBirth;
+  }
+};
+*/
+
+console.log(james);
+
+//console.log(james.calculateAge(1990));
+/*console.log(james.calculateAge());
+var age = james.calculateAge();
+james.age = age;
+*/
+//v2.0
+var james = {
+  name : 'James',
+  lastName : 'Smith',
+  yearOfBirth : 1990,
+  job : 'Doctor',
+  isMarried : false,
+  family : ['Jane' , 'Mark' , 'Bob'],
+  calculateAge : function(){
+    this.age =  2018 - this.yearOfBirth;
+  }
+};
+
+
+james.calculateAge();
+console.log(james);
+
+var mike = {
+  name : 'Mike',
+  lastName : 'Smith',
+  yearOfBirth : 1950,
+  job : 'Doctor',
+  isMarried : false,
+  family : ['Jane' , 'Mark' , 'Bob'],
+  calculateAge : function(){
+    this.age =  2018 - this.yearOfBirth;
+  }
+};
+
+
+mike.calculateAge();
+console.log(mike);
+
+
+for(var i = 1;i<=10;i++){
+  console.log(i);
+}
+console.log("00000000");
+// var yearsArray = [2005,1994,1995,1996,2000];
+// var ageArray = [];
+// function calcAge(age){
+//   return 2018-age;
+// }
+// for(var i=0;i<yearsArray.length;i++){
+//   ageArray[i] = calcAge(yearsArray[i]);
+//   //console.log(ageArray[i]);
+//   if(ageArray[i]>=18){
+//     console.log("Age "+ageArray[i]+" is eligible to vote");
+//   }else{
+//     console.log('Age '+ageArray[i]+' is not eligible to vote');
+//   }
+// }
+
+
+calcAge(1995);
+function calcAge(age) {
+  console.log(2018-age);
+}
+console.log("(^_^)");
+
+var a = 'A';
+first();
+function first(){
+  var b = 'B';
+  second();
+  function second(){
+    var c = 'C';
+    third();
+  }
+}
+function third(){
+  var d ='D';
+  console.log(a+d);
+}
+
+
+var jo ={
+  name : 'John',
+  year : 1990,
+age :function(){
+  console.log(this);
+  console.log(2018-this.year);
+  function addInner(){
+    console.log(this);
+  }
+//  addInner();
+}
+}
+jo.age();
+
+var mi = {
+  name :'Mike',
+  year : 1999
+};
+mi.age = jo.age;
+mi.age();
