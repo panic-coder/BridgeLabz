c505a185fcf19bd613a4dcaf244f26c838dfbe22
zombie24x7 <androidshubham@gmail.com>
2018-06-19 14:39:59 +0530

    Login

--- Web/ng5/src/app/app.module.ts
+++ Web/ng5/src/app/app.module.ts
@@ -4,20 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from "@angular/material/icon";
-// import { Ng5Module } from './ng5.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatMenuModule, MatButtonModule, MatCardModule, MatSidenavModule,MatListModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule,MatInputModule, MatButtonModule, MatCardModule, MatSidenavModule,MatListModule } from '@angular/material';
 import { HeroService } from './hero.service';
+import { Routes, RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+    { path: '', component:LoginComponent },
+    { path: 'header', component:HeaderComponent}
+];
 
 @NgModule({
     declarations:[
         AppComponent,
         HeaderComponent,
-        SidenavComponent
+        SidenavComponent,
+        LoginComponent
     ],
     imports:[
         BrowserModule,
@@ -26,7 +35,12 @@ import { SidenavComponent } from './sidenav/sidenav.component';
         BrowserAnimationsModule,
         MatToolbarModule,
         MatIconModule,
-        MatMenuModule,MatButtonModule,MatCardModule,MatSidenavModule,MatListModule
+        MatInputModule,
+        MatMenuModule,MatButtonModule,MatCardModule,MatSidenavModule,MatListModule,
+        RouterModule.forRoot(appRoutes),
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        
     ],
     providers:[HeroService],
     bootstrap:[AppComponent]
